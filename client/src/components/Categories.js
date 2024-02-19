ef3c5bf64a60a19807efcf5c0d4dec5fc3c57cc0
Alexander <lucker631@gmail.com>
2024-02-19 11:41:51 +0100

    Delete

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useStripe } from "@stripe/react-stripe-js";
 
 function Categories() {
   const [categories, setCategories] = useState(null);
